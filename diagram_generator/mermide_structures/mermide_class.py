2bb32df62bf5e6f37183a61d9e9a761da65190b4
jesushledon@gmail.com <jesushledon@gmail.com>
2022-06-27 20:16:23 -0500

    Creating string body structires

---
+++ diagram_generator/mermide_structures/mermide_class.py
@@ -0,0 +1,10 @@
+VISIBILITY={
+    "public ": '+',
+    "private": "-",
+    "protected": "#",
+    "package": "~"
+}
+
+ATTRIBUTE = "{visibility}{attribute_type}{attribute_generic_type} {attribute_name}"
+METHOD = "{visibility}{method_name}({method_arguments}) {method_return_type}"
+CLASS_DEFINITION = "class {class_name}{parent_class}"
